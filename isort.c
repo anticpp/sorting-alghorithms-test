6b71e8a1c10a8b358c4d1bc28df86e24ea2f7d5a
supergui <supergui@live.cn>
2022-09-27 11:34:12 +0800

    improve isort

--- isort.c
+++ isort.c
@@ -2,16 +2,19 @@
  */
 void isort(int arr[], int start, int end) {
     for(int i=start+1; i<=end; i++) {
-        int v = arr[i];
-        int j = i-1;
-        for(; j>=start; j--) {
+        // Save arr[i] as sentinal to avoid swap values
+        int snl = arr[i];
+        int j = i;
+        for(; j>start; j--) {
             cnt_inc_comp();
-            if(v<arr[j]) {
-                arr[j+1] = arr[j];
+            if(snl<arr[j-1]) {
+                // Move [j-1] => [j]
+                arr[j] = arr[j-1];
             } else {
                 break;
             }
         }
-        arr[j+1] = v;
+        // Keep sentinal value to arr[j]
+        arr[j] = snl;
     }
 }
